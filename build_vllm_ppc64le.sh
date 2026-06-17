363c0fd7ff503067ece0866d1af924c6d861d84e
Puneet Sharma <puneet.sharma21@ibm.com>
2026-06-17 09:58:48 +0530

    Update build_vllm_ppc64le.sh

--- build_vllm_ppc64le.sh
+++ build_vllm_ppc64le.sh
@@ -249,3 +249,5 @@ export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/local/lib64/pkgconfig:/usr/
 uv pip install -r requirements/common.txt \
                -r requirements/cpu.txt \
                -r requirements/build/cpu.txt --extra-index-url "$IBM_DEVPI_URL" --index-strategy unsafe-best-match
+
+uv pip install --upgrade transformers
